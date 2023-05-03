51c8505e151be01465281c5c9285fca51502e1b9
Oryatory <larin.ia28@gmail.com>
2023-05-03 22:57:51 +0400

    image-fix-2

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -16,7 +16,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance 574 Vintage Brights",
     price: 65000,
-    image: "../src/assets/images/cm996shb_nb_02_i 1.png",
+    image: "../images/cm996shb_nb_02_i 1.png",
     id: "MT91547",
     amount: 0,
     productIsOpen: false,
@@ -25,7 +25,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance Made in UK 920 Chinese New Year",
     price: 120000,
-    image: "../src/assets/images/cm996shb_nb_02_i 2.png",
+    image: "../images/cm996shb_nb_02_i 2.png",
     id: "MT91548",
     amount: 0,
     productIsOpen: false,
@@ -34,7 +34,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance 373 Modern Classics",
     price: 80000,
-    image: "../src/assets/images/cm996shb_nb_02_i 3.png",
+    image: "../images/cm996shb_nb_02_i 3.png",
     id: "MT91549",
     amount: 0,
     productIsOpen: false,
@@ -43,7 +43,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance Made in UK 670 Chinese New Year",
     price: 78000,
-    image: "../src/assets/images/cm996shb_nb_02_i 4.png",
+    image: "../images/cm996shb_nb_02_i 4.png",
     id: "MT91550",
     amount: 0,
     productIsOpen: false,
@@ -52,7 +52,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance X-Racer Utility",
     price: 100000,
-    image: "../src/assets/images/cm996shb_nb_02_i 5.png",
+    image: "../images/cm996shb_nb_02_i 5.png",
     id: "MT91551",
     amount: 0,
     productIsOpen: false,
@@ -61,7 +61,7 @@ export const products: ProductProps[] = [
   {
     name: "New Balance 5740 Think Colorfully",
     price: 94000,
-    image: "../src/assets/images/cm996shb_nb_02_i 6.png",
+    image: "../images/cm996shb_nb_02_i 6.png",
     id: "MT91552",
     amount: 0,
     productIsOpen: false,

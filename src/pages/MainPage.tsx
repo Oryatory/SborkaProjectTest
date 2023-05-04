b335eee7be793af6a14ad0d115c43ef9928079a7
Oryatory <larin.ia28@gmail.com>
2023-05-04 18:57:00 +0400

    route-fix

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,8 +2,6 @@ import { FC } from "react";
 import Header from "../components/Header";
 import Main from "../components/Main";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ProductsList from "../components/ProductsList";
-import SingleProductPage from "../components/SingleProductPage";
 
 const MainPage: FC = () => {
   return (

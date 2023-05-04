2650e54eb9aa7c1af1e934ca07e70eebd2267020
Oryatory <larin.ia28@gmail.com>
2023-05-05 00:54:43 +0400

    loading removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import { FC, useState, useEffect } from "react";
 import MainPage from "./pages/MainPage";
-import Loading from "./components/Loading";
 
 const App: FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const handleLoading = () => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 1000);
-  };
-
-  useEffect(() => {
-    document.addEventListener("DOMContentLoaded", handleLoading);
-    window.addEventListener("load", handleLoading);
-
-    return () => {
-      document.removeEventListener("DOMContentLoaded", handleLoading);
-      window.removeEventListener("load", handleLoading);
-    };
-  }, []);
-
-  return <>{isLoading ? <Loading /> : <MainPage />}</>;
+  return <MainPage />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./assets/fonts/Roboto-Regular.ttf";
+import "/fonts/Roboto-Regular.woff2";
 import "./scss/index.scss";
 import { CartProvider } from "./context/cart_context.tsx";
 import { GlobalProvider } from "./context/useGlobalContext.tsx";
